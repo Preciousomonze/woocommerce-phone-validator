1d6dd24faf8a93f51b9bdee53a07ad430fa7a05d
Tim Norris <tibben@ocf.berkeley.edu>
2019-11-09 18:17:29 -0500

    check for validator fields in $_POST
    This avoids throwing a php warning if either one of the validator fields is not present in the $_POST

--- public/class-woocommerce-account.php
+++ public/class-woocommerce-account.php
@@ -23,9 +23,9 @@ Class WC_PV_Account{
         global $wc_pv_woo_custom_field_meta;
         $phone_name = $wc_pv_woo_custom_field_meta['billing_hidden_phone_field'];
         $phone_err_name = $wc_pv_woo_custom_field_meta['billing_hidden_phone_err_field'];
-        $phone_valid_field = strtolower( sanitize_text_field($_POST[$phone_name]) );
-        $phone_valid_err_field = trim( sanitize_text_field( $_POST[$phone_err_name] ) );
-		$bil_email = sanitize_email($_POST['billing_email']);
+        $phone_valid_field = isset( $_POST[$phone_name] ) ? strtolower( sanitize_text_field($_POST[$phone_name]) ) : '';
+        $phone_valid_err_field = isset( $_POST[$phone_err_name] ) ? trim( sanitize_text_field( $_POST[$phone_err_name] ) ) : '';
+	$bil_email = sanitize_email($_POST['billing_email']);
         $bil_phone = sanitize_text_field($_POST['billing_phone']);
 
         if( !empty($bil_email) && !empty($bil_phone) && (empty($phone_valid_field) || !is_numeric($phone_valid_field) ) ){//there was an error, this way we know its coming directly from normal woocommerce, so no conflict :)
