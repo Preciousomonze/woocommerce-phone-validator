de4ce7d8b4366abac8ec2e57e1df8d992a54d1d3
Preciousomonze <crispusomonze@gmail.com>
2020-07-24 00:54:35 +0100

    errMsg rerendering properly,little linting

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -9,7 +9,7 @@ if($('.wc-pv-intl input').length == 0){//add class, some checkout plugin has ove
     $('#billing_phone_field').addClass('wc-pv-phone wc-pv-intl');
 }
 // Set default country.
-var wcPvDefCountry = ( wcPvJson.defaultCountry === '' ? $( `${wcPvJson.parentPage} #billing_country` ).val() : wcPvJson.defaultCountry );
+var wcPvDefCountry = ( wcPvJson.defaultCountry === '' ? $( `${wcPvJson.parentPage} #biling_country` ).val() : wcPvJson.defaultCountry );
 
 let separateDialCode = ( wcPvJson.separateDialCode == 1 ? true : false );
 let onlyCountries = wcPvJson.onlyCountries.map( value => { return value.toUpperCase(); } );
@@ -17,21 +17,23 @@ let onlyCountries = wcPvJson.onlyCountries.map( value => { return value.toUpperC
 var wcPvPhoneIntl = $('.wc-pv-intl input').intlTelInput({
     initialCountry: ( wcPvDefCountry == '' ? 'NG' : wcPvDefCountry ),
     onlyCountries: onlyCountries,
-    /*geoIpLookup: function(callback) {
+    separateDialCode: separateDialCode, 
+    utilsScript: wcPvJson.utilsScript,
+    //autoHideDialCode: true,
+    //nationalMode: false,
+    /* geoIpLookup: function(callback) {
     $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
     const countryCode = (resp && resp.country) ? resp.country : '';//asking for payment shaa,smh
     callback(countryCode);
     });
-    },//to pick user country*/
-    separateDialCode: separateDialCode, 
-    utilsScript: wcPvJson.utilsScript
+    },//to pick user country */
 });
 
 /*if(wcPvJson.userPhone !== undefined ){
     wcPvPhoneIntl.intlTelInput("setNumber").val(wcPvJson.userPhone);
 }*/
 
-// Some Globals
+// Some Globals.
 var wcPvphoneErrMsg = '';
 
 /**
@@ -40,21 +42,21 @@ var wcPvphoneErrMsg = '';
  * @param intlTelInput input
  * @return string or bool
  */
-function wcPvValidatePhone(input){
+function wcPvValidatePhone( input ) {
     const phone = input;
     let result = false;
-    if(phone.intlTelInput('isValidNumber') == true){
-        result = phone.intlTelInput('getNumber');
+    if ( phone.intlTelInput('isValidNumber') == true ) {
+        result = phone.intlTelInput( 'getNumber' );
     }
     else{
-        let errorCode = phone.intlTelInput('getValidationError');
-        wcPvphoneErrMsg = ` ${wcPvJson.phoneErrorTitle + (wcPvPhoneErrorMap[errorCode] == undefined ? wcPvJson.phoneUnknownErrorMsg : wcPvPhoneErrorMap[errorCode])}`;
+        let errorCode = phone.intlTelInput( 'getValidationError' );
+        wcPvphoneErrMsg = `${wcPvJson.phoneErrorTitle + (wcPvPhoneErrorMap[errorCode] == undefined ? wcPvJson.phoneUnknownErrorMsg : wcPvPhoneErrorMap[errorCode])}`;
     }
     return result;
 }
 
 // Incase of country change
-$( `${wcPvJson.parentPage} #billing_country` ).change( function() {
+$ ( `${wcPvJson.parentPage} #billing_country` ).change( function() {
     let value = $( this ).val();
 
     // Make sure you only set if its in the selected countries
@@ -62,47 +64,48 @@ $( `${wcPvJson.parentPage} #billing_country` ).change( function() {
         wcPvPhoneIntl.intlTelInput( 'setCountry', value );
     }
 });
-// Adjust design if true
-if( separateDialCode == true ){
-    $('.wc-pv-intl').addClass( 'wc-pv-separate-dial-code' );
+
+// Adjust design if true.
+if ( separateDialCode === true ) {
+    $( '.wc-pv-intl' ).addClass( 'wc-pv-separate-dial-code' );
 }
+
 /**
  *  Js validation process
  * @param {object} parentEl the parent element 
  */
-function wcPvValidateProcess(parentEl){
-    let phoneNumber = wcPvValidatePhone(wcPvPhoneIntl);
-    if($('.wc-pv-intl input').length == 0)//doesnt exist, no need
+function wcPvValidateProcess( parentEl ) {
+    let phoneNumber = wcPvValidatePhone( wcPvPhoneIntl );
+    if ( $( '.wc-pv-intl input' ).length == 0) // Doesnt exist, no need.
         return;
 
     // Remove errors first, so its not stagnant, special thanks to Sylvain :)
     $('#wc-ls-phone-valid-field-err-msg').remove();
 
-    if(phoneNumber != false){//phone is valid
-        $(`${wcPvJson.parentPage} input#billing_phone`).val(phoneNumber);//set the real value so it submits it along
-        if($('#wc-ls-phone-valid-field').length == 0){//append
+    if( phoneNumber != false ){ // Phone is valid.
+        $( `${wcPvJson.parentPage} input#billing_phone` ).val( phoneNumber ); // Set the real value so it submits it along.
+
+        if ( $( '#wc-ls-phone-valid-field' ).length == 0 ) { // Append.
             parentEl.append(`<input id="wc-ls-phone-valid-field" value="${phoneNumber}" type="hidden" name="${wcPvJson.phoneValidatorName}">`);
         }
-    //    $('#wc-ls-phone-valid-field-err-msg').remove();
     }
     else{
-        if($('#wc-ls-phone-valid-field-err-msg').length == 0){//append
-            parentEl.append(`<input id="wc-ls-phone-valid-field-err-msg" value="${wcPvphoneErrMsg}" type="hidden" name="${wcPvJson.phoneValidatorErrName}">`);
-        }
-        $('#wc-ls-phone-valid-field').remove();
+        parentEl.append( `<input id="wc-ls-phone-valid-field-err-msg" value="${wcPvphoneErrMsg}" type="hidden" name="${wcPvJson.phoneValidatorErrName}">` );
+        $( '#wc-ls-phone-valid-field' ).remove();
     }
 }
-//for woocommerce checkout
-if(wcPvJson.currentPage == 'checkout'){
-    let wcPvCheckoutForm = $(`${wcPvJson.parentPage}`);
-    wcPvCheckoutForm.on('checkout_place_order',function(){
-        wcPvValidateProcess(wcPvCheckoutForm);
+
+// For woocommerce checkout.
+if( wcPvJson.currentPage == 'checkout' ) {
+    let wcPvCheckoutForm = $( `${wcPvJson.parentPage}` );
+    wcPvCheckoutForm.on( 'checkout_place_order', function(){
+        wcPvValidateProcess( wcPvCheckoutForm );
     });
 }
-else if(wcPvJson.currentPage == 'account'){//for account page
+else if( wcPvJson.currentPage == 'account') { // For account page.
     let wcPvAccForm = $(`${wcPvJson.parentPage} form`);
-    $(`${wcPvJson.parentPage}`).submit(function(){
-        wcPvValidateProcess(wcPvAccForm);
+    $( `${wcPvJson.parentPage}` ).submit( function(){
+        wcPvValidateProcess( wcPvAccForm );
     });
 }
 
