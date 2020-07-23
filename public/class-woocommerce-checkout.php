8e4ce5d78760d1a213d80dcc3a7b1b40f3e0fc34
Preciousomonze <crispusomonze@gmail.com>
2020-07-24 00:55:18 +0100

    fix:hide our err if wc_phone error handles basics"

--- public/class-woocommerce-checkout.php
+++ public/class-woocommerce-checkout.php
@@ -95,12 +95,12 @@ Class WC_PV_Checkout{
         $bil_phone = sanitize_text_field($_POST['billing_phone']);
 
        if( !empty($bil_email) && !empty($bil_phone) && (!empty($phone_valid_err_field)) && (empty($phone_valid_field) || !is_numeric($phone_valid_field) ) ){//there was an error, this way we know its coming directly from normal woocommerce, so no conflict :)
-            if( !is_numeric( $bil_phone ) ) // WC will handle this, so no need to report errors
-                return;
+            if( !is_numeric( str_replace( ' ', '', $bil_phone ) ) ) // WC will handle this, so no need to report errors
+               return;
 
-            $ph = explode(':',$phone_valid_err_field);
-            $ph[0] = '<strong>'.$ph[0].'</strong>';
-            $phone_err_msg = implode(':',$ph);
+            $ph = explode( ':', $phone_valid_err_field );
+            $ph[0] = '<strong>' . $ph[0] . '</strong>';
+            $phone_err_msg = implode( ':', $ph );
             $out =  __($phone_err_msg, 'woo-phone-validator' );
 			wc_add_notice( $out, 'error');
         }
